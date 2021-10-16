b8f9b68b85f9f96c33b68d035dc0067cdd690acd
Justin Geschiere <j.geschiere@hotmail.com>
2021-10-16 21:52:13 +0200

    Add async mediator extensions

--- Timeline.Vertical.Web/Extensions/MediatorExtensions.cs
+++ Timeline.Vertical.Web/Extensions/MediatorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Timeline.Vertical.Features.Interfaces;
 
 namespace Timeline.Vertical.Web.Extensions
@@ -10,14 +11,29 @@ namespace Timeline.Vertical.Web.Extensions
 			return controller.View(feature.Execute(command));
 		}
 
+		public static async Task<IActionResult> ExecuteView<TCommand, TResult>(this Controller controller, IFeatureAsync<TCommand, TResult> feature, TCommand command)
+		{
+			return controller.View(await feature.ExecuteAsync(command));
+		}
+
 		public static IActionResult ExecuteOk<TCommand, TResult>(this Controller controller, IFeature<TCommand, TResult> feature, TCommand command)
 		{
 			return controller.Ok(feature.Execute(command));
 		}
 
+		public static async Task<IActionResult> ExecuteOk<TCommand, TResult>(this Controller controller, IFeatureAsync<TCommand, TResult> feature, TCommand command)
+		{
+			return controller.Ok(await feature.ExecuteAsync(command));
+		}
+
 		public static IActionResult ExecuteContent<TCommand, TResult>(this Controller controller, IFeature<TCommand, TResult> feature, TCommand command)
 		{
 			return controller.Content(feature.Execute(command).ToString());
+		}
+
+		public static async Task<IActionResult> ExecuteContent<TCommand, TResult>(this Controller controller, IFeatureAsync<TCommand, TResult> feature, TCommand command)
+		{
+			return controller.Content((await feature.ExecuteAsync(command)).ToString());
 		}
 	}
 }
